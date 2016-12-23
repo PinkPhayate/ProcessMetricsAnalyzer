eb6abd8498c45243bd685ac248fcb6ce3ffef197
Tanaka Hayate <pinqphayate@gmail.com>
2016-12-23 23:21:26 +0900

    modify module

--- src/main/Module.java
+++ src/main/Module.java
@@ -1,15 +1,7 @@
 package main;
 
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.TreeMap;
-
-import lib.FileReading;
 
 public class Module {
 	private String fileName;
@@ -17,20 +9,17 @@ public class Module {
 	
 	private int begenningPosition = -1;
 	private int endingPosition = -1;
-
 	
-	private int totalLine ;
-	private int deletedLine = 0;
-	private int newLine = 0;
-	private int changedLine = 0;
-	
-	private double chum = 0;
-	private double relativeChum = 0;
-	private double deletedChum = 0;
-	private double ncdChum = 0;
+	private double M1 = 0;
+	private double M2 = 0;
+	private double M3 = 0;
+	private double M4 = 0;
+	private double M5 = 0;
+	private double M6 = 0;
+	private double M7 = 0;
+	private double M8 = 0;
 
 	private int isNewModule = 0;
-	private int numOfBug = 0;
 	
 	private static String mainDir ;
 	
@@ -58,117 +47,26 @@ public class Module {
 		return this.endingPosition;
 	}
 
-	public void adddeletedLine(int deletedLine) {
-		this.deletedLine += deletedLine ;
-	}
-	public void addTotalLine(int totalLine) {
-		this.totalLine = totalLine+1 ;
-	}
-	public void addNewLine(int newLine) {
-		this.newLine += newLine ;
-	}
-	public void addChangeLine(int changedLine) {
-		this.changedLine += changedLine ;
-	}
-	public void addNumOfBug(){
-		this.numOfBug++;
-	}
-	public void putLOC(int totalLine) {
-		this.totalLine = totalLine;
-	}
 	public void isNew() {
 		this.isNewModule = 1;
 	}
-//	public String putMetricses(Module module) {
-//		String str_numOfBug = String.valueOf(module.numOfBug) ;
-//		String str_totalLine = String.valueOf(module.totalLine) ;
-//		String str_chum = String.valueOf(module.chum) ;
-//		String str_relativeChum = String.valueOf(module.relativeChum) ;
-//		String str_deletedChum = String.valueOf(module.deletedChum) ;
-//		String str_ncdChum = String.valueOf(module.ncdChum) ;
-//		String metricses = 
-//				str_numOfBug + ","
-//				+ str_totalLine + ","
-//				+ str_chum + ","
-//				+ str_relativeChum + ","
-//				+ str_deletedChum + ","
-//				+ str_ncdChum ;
-//		
-//		return metricses ;
-//				
-//}
-	public String getMetrics() {
-//		this.circulateMetricses();
-		String metricses = 
-				String.valueOf(this.totalLine) + ","
-				+ String.valueOf(this.chum) + ","
-				+ String.valueOf(this.relativeChum) + ","
-				+ String.valueOf(this.deletedChum) + ","
-				+ String.valueOf(this.ncdChum) + ","
-				+ String.valueOf(this.isNewModule);
-		return metricses ;
-		
-	}
+
 	public void calculateMetrics(TreeMap<String,Integer> differences) {
 		int totalLine = differences.get("totalLine");
 		int newLine = differences.get("cntNew");
 		int changedLine = differences.get("cntChange");
 		int deletedLine = differences.get("cntDelete");
-// TODO	calculate process metrics
-
+		
+		this.M1 = newLine + changedLine;
+		this.M2 = deletedLine / totalLine;
+		this.M6 = (newLine + changedLine + deletedLine);
+		this.M7 = (newLine + changedLine) / deletedLine;
 		
 	}
-	public void circulateMetricses (TreeMap<String,Integer> differences) {
-		this.totalLine = differences.get("totalLine");
-		this.newLine = differences.get("newLine");
-		this.changedLine = differences.get("changedLine");
-		this.deletedLine = differences.get("deletedLine");
-
-		if ( this.changedLine != 0 ) {
-			this.chum = this.newLine + this.changedLine;
-		}
-		if ( this.totalLine != 0 ) {
-			this.relativeChum = (double) (this.newLine + this.changedLine) / this.totalLine ;
-			this.deletedChum = (double) this.deletedLine / this.totalLine ;
-		}
-		if ( this.deletedLine != 0 ) {
-			this.ncdChum = (double) (this.newLine + this.changedLine) / this.deletedLine ;
-		}
-
+	public String getMetricsList () {
+		return fileName + "," +className + "," + isNewModule + "," + M1 + "," + M2 + "," + M6 + "," + M7;
 	}
-
 	
-//	public Module(String version, String mainDir) {
-//		this.version = version ;
-//		this.mainDir = mainDir;
-//	}
-	
-//	public static void importModuleList() throws IOException {
-///**	String mainDir ="C:\\Users\\OWNER\\Documents\\Derby\\" + "db-derby-"+version + "-bin
-//*/
-//		File file = new File(mainDir + "\\lib\\v"+version+"_moduleList_cls.txt");
-//		FileReader fr = new FileReader(file);
-//		BufferedReader br = new BufferedReader(fr) ;
-//		String line = br.readLine() ;
-//		while(line != null) {
-//			Module module = new Module(version,mainDir);
-//			moduleMap.put(line, module);
-//			line= br.readLine();
-//		}
-//		
-//		br.close();
-//	}
-//	public static boolean isExist(String moduleName) {
-//		Module module = moduleMap.get(moduleName);
-//		if(module == null) {
-//			return false;
-//		}
-//		return true;
-//	}
-//	public String getModuleData() {
-//		String metricses = String.valueOf(chum) +"," + String.valueOf(relativeChum) +"," + String.valueOf(deletedChum) +"," + String.valueOf(ncdChum) ;
-//		return name +"," +metricses +"," + this.numOfBug;
-//	}
-
 
 }
+
