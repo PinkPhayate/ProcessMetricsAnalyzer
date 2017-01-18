26dcd80a13423df199a602454686768b02405879
Tanaka Hayate <pinqphayate@gmail.com>
2017-01-19 01:27:32 +0900

    add script to select directory only

--- src/GUI/JFileChooserTest5.java
+++ src/GUI/JFileChooserTest5.java
@@ -7,54 +7,77 @@ import java.awt.event.*;
 
 public class JFileChooserTest5 extends JFrame implements ActionListener{
 
-  JLabel label;
+	JLabel label;
+	JPanel buttonPanelLower  = null;
+	JButton buttonUpper = null;
+	JButton buttonLower = null;
+	JTextField upperTtext  = null;
+	JTextField lowerText  = null;
 
-  public static void main(String[] args){
-    JFileChooserTest5 frame = new JFileChooserTest5();
+	public static void main(String[] args){
+		JFileChooserTest5 frame = new JFileChooserTest5();
 
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setBounds(10, 10, 300, 200);
-    frame.setTitle("DIMA");
-    frame.setVisible(true);
-  }
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(10, 10, 300, 200);
+		frame.setTitle("DIMA");
+		frame.setVisible(true);
+	}
 
-  JFileChooserTest5(){
+	JFileChooserTest5(){
+		label = new JLabel();
+		JPanel labelPanel = new JPanel();
+		labelPanel.add(label);
+		getContentPane().add(labelPanel, BorderLayout.CENTER);
 
 
-    label = new JLabel();
-    JPanel labelPanel = new JPanel();
-    labelPanel.add(label);
-    getContentPane().add(labelPanel, BorderLayout.CENTER);
-    
-    JPanel buttonPanel = new JPanel();
-    buttonPanel.setLayout(null);
-    JButton button = new JButton("file select");
-    button.setBounds(200,10,80,20);
-    button.addActionListener(this);
-    buttonPanel.add(button);
-    getContentPane().add(buttonPanel, BorderLayout.CENTER);
-    
-    JPanel buttonPanelLower = new JPanel();
-    buttonPanelLower.setLayout(null);
-    JButton buttonLower = new JButton("file select");
-    buttonLower.setBounds(200,50,80,20);
-    buttonLower.addActionListener(this);
-    buttonPanel.add(buttonLower);
-//    getContentPane().add(buttonPanelLower, BorderLayout.CENTER);
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(null);
+		this.buttonUpper = new JButton("directory1");
+		this.buttonUpper.setBounds(200,10,80,20);
+		this.buttonUpper.addActionListener(this);
+		buttonPanel.add(this.buttonUpper);
+		getContentPane().add(buttonPanel, BorderLayout.CENTER);
 
-  }
+		JPanel p = new JPanel();
 
-  public void actionPerformed(ActionEvent e){
-    JFileChooser filechooser = new JFileChooser();
+		this.upperTtext = new JTextField("");
+		this.upperTtext.setBounds(0, 10, 200, 20);
+		buttonPanel.add(this.upperTtext);    
+		this.upperTtext.setLayout(null);
 
-    int selected = filechooser.showOpenDialog(this);
-    if (selected == JFileChooser.APPROVE_OPTION){
-      File file = filechooser.getSelectedFile();
-      label.setText(file.getName());
-    }else if (selected == JFileChooser.CANCEL_OPTION){
-      label.setText("キャンセルされました");
-    }else if (selected == JFileChooser.ERROR_OPTION){
-      label.setText("エラー又は取消しがありました");
-    }
-  }
+
+		buttonPanelLower = new JPanel();
+		buttonPanelLower.setLayout(null);
+		buttonLower = new JButton("directory2");
+		buttonLower.setBounds(200,50,80,20);
+		buttonLower.addActionListener(this);
+		buttonPanel.add(buttonLower);
+		
+		this.lowerText = new JTextField("");
+		this.lowerText.setBounds(0, 50, 200, 20);
+		buttonPanel.add(this.lowerText);    
+		this.lowerText.setLayout(null);
+
+	}
+
+	public void actionPerformed(ActionEvent e){
+		JFileChooser filechooser = new JFileChooser();
+		filechooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int selected = filechooser.showOpenDialog(this);
+		if (selected == JFileChooser.APPROVE_OPTION){
+			File file = filechooser.getSelectedFile();
+			if(e.getSource() == this.buttonUpper){
+				upperTtext.setText(file.getName());
+			}
+			if(e.getSource() == this.buttonLower){
+				lowerText.setText(file.getName());
+			}
+		}
+
+		//    }else if (selected == JFileChooser.CANCEL_OPTION){
+		//      label.setText("キャンセルされました");
+		//    }else if (selected == JFileChooser.ERROR_OPTION){
+		//      label.setText("エラー又は取消しがありました");
+		//    }
+	}
 }
